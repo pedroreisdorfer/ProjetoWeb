e416d869279df7c6b6ce46ff80d7d7853bcd060f
Pedro Vinícius Francisco Reisdorfer <vinireisdorfer@gmail.com>
2022-04-20 13:22:13 -0300

    Preparing sales search navigation views

---
+++ WebService/Controllers/SalesRecordsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService.Controllers
+{
+    public class SalesRecordsController : Controller // a pasta criada na Views precisa ter o mesmo nome, no caso: SalesRecords
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
