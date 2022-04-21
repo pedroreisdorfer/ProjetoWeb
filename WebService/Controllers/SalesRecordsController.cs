17985b2f9585fd2c0db94f4cde7272ff56ab83ed
Pedro Vinícius Francisco Reisdorfer <vinireisdorfer@gmail.com>
2022-04-20 22:01:57 -0300

    Implementing simple search

--- WebService/Controllers/SalesRecordsController.cs
+++ WebService/Controllers/SalesRecordsController.cs
@@ -3,19 +3,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebService.Services;
 
 namespace WebService.Controllers
 {
     public class SalesRecordsController : Controller // a pasta criada na Views precisa ter o mesmo nome, no caso: SalesRecords
     {
+        private readonly SalesRecordService _salesRecordService; // criação de dependência //
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue) // teste se minha data mínima possui um valor.No caso, "Se não possuir um valor mínimo:
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1); // recebe uma data, qual? dia primeiro de janeiro do ano atual
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd"); // passando esses dados para minha View por meio da ViewData
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
         }
 
         public IActionResult GroupingSearch()
---
+++ WebService/Services/SalesRecordService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebService.Data;
+using WebService.Models;
+
+namespace WebService.Services
+{
+    public class SalesRecordService
+    {
+        private readonly WebServiceContext _conetext; // criando assim uma dependência do nosso DbContext //
+
+        public SalesRecordService(WebServiceContext context) // construtor para que injeção de dependência possa ocorrer //
+        {
+            _conetext = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate) // essa operação vai receber duas datas, data mínima e data máxima. No caso, as datas são opcionais
+        {
+            var result = from obj in _conetext.SalesRecord select obj; // lógica para encontrar as vendas em um dado intervalo de data // foi criado objeto para construir consultas em cima dele a partir do DbContext do SalesRecord //
+            // essa declaração vai pegar o SalesRecord do tipo DbSet e construir um objeto result do tipo IQueryable //
+            if (minDate.HasValue) // se informei uma data mínima
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }                        // expressão lambda que expresse minha restrição de data
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= maxDate.Value);
+            }
+            // para executar minha consulta:
+            return await result
+                .Include(x => x.Seller) // faz o join das tabelas
+                .Include(x => x.Seller.Department) // faço o Join com a tabela de departamentos
+                .OrderByDescending(x => x.Date) // ordena por data em ordem descrescente
+                .ToListAsync();
+        }
+
+    }
+}
